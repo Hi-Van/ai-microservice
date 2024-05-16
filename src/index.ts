aa6d0b2cd00b4a7238ed7c18728488207e124481
Hi-Van <sirisoukvanouphon@gmail.com>
2024-05-16 16:28:15 -0500

    dockerize and add cors

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import express, { Request, Response } from "express";
+import cors from "cors";
 import v1Router from "./routes/v1";
 
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+const allowedOrigins = ["http://localhost:3000", "https://vansiriso.uk"];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(options));
 app.use(express.json());
 app.use("/v1", v1Router);
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript Express!");
+  res.status(200).send("Hello, TypeScript Express!");
 });
 
 app.listen(port, () => {
